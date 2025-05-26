05ceb79d6d07d6ce4d6c6ef022b6251ae92824e7
YCheng <briansam195@gmail.com>
2025-05-26 16:18:44 +0800

    測試BeforeAll、AfterAll、BeforeEach、AfterEach、Disabled、DisplayName註解

--- src/test/java/com/example/springboot_test/CalculatorTest.java
+++ src/test/java/com/example/springboot_test/CalculatorTest.java
@@ -1,11 +1,32 @@
 package com.example.springboot_test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("使用 beforeAll");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("使用 afterAll");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("使用 beforeEach");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("使用 afterEach");
+    }
+
+    @Disabled
     @Test
     public void test() {
         Calculator calculator = new Calculator();
@@ -13,5 +34,13 @@ class CalculatorTest {
         int result = calculator.add(1, 2);
 
         assertEquals(3, result);
+
+        System.out.println("驗證加法");
+    }
+
+    @DisplayName("測試 test2")
+    @Test
+    public  void test2() {
+        System.out.println("執行 test2");
     }
 }
